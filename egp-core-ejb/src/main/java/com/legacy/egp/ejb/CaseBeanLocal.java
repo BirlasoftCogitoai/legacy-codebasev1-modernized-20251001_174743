c952e973396597e7b755c6b1d899e23099051dc0
acm15aug-a11y <acm15aug@gmail.com>
2025-10-01 17:47:59 +0530

    Add egp-core-ejb/src/main/java/com/legacy/egp/ejb/CaseBeanLocal.java

---
+++ egp-core-ejb/src/main/java/com/legacy/egp/ejb/CaseBeanLocal.java
@@ -0,0 +1,28 @@
+```java
+package com.legacy.egp.ejb;
+
+import com.legacy.egp.entity.CaseRecord;
+
+import javax.ejb.Local;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Modernized Case Bean Local Interface
+ */
+@Local
+public interface CaseBeanLocal {
+
+    CaseRecord createCase(CaseRecord caseRecord);
+
+    CaseRecord updateCase(CaseRecord caseRecord);
+
+    Optional<CaseRecord> getCase(Long caseId);
+
+    Optional<CaseRecord> getCaseByCaseNumber(String caseNumber);
+
+    List<CaseRecord> getCasesByCustomer(Long customerId);
+
+    List<CaseRecord> getAllCases();
+}
+```
