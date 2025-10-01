3b072d66c88681112553128f356fee0995d814e7
acm15aug-a11y <acm15aug@gmail.com>
2025-10-01 17:48:05 +0530

    Add egp-core-ejb/src/main/java/com/legacy/egp/ejb/CustomerBeanRemote.java

--- egp-core-ejb/src/main/java/com/legacy/egp/ejb/CustomerBeanRemote.java
+++ egp-core-ejb/src/main/java/com/legacy/egp/ejb/CustomerBeanRemote.java
@@ -1,30 +1,49 @@
-```java
 package com.legacy.egp.ejb;
 
 import com.legacy.egp.entity.Customer;
 
 import javax.ejb.Remote;
 import java.util.List;
-import java.util.Optional;
 
 /**
- * Modernized Customer Bean Remote Interface
+ * Customer Bean Remote Interface
+ * Provides remote access to customer-related operations
  */
 @Remote
 public interface CustomerBeanRemote {
 
+    /**
+     * Creates a new customer
+     * @param customer the customer to create
+     * @return the created customer
+     */
     Customer createCustomer(Customer customer);
 
+    /**
+     * Updates an existing customer
+     * @param customer the customer to update
+     * @return the updated customer
+     */
     Customer updateCustomer(Customer customer);
 
-    Optional<Customer> getCustomer(Long customerId);
+    /**
+     * Retrieves a customer by ID
+     * @param customerId the ID of the customer to retrieve
+     * @return the retrieved customer
+     */
+    Customer getCustomer(Long customerId);
 
-    Optional<Customer> getCustomerByNumber(String customerNumber);
+    /**
+     * Retrieves a customer by customer number
+     * @param customerNumber the customer number of the customer to retrieve
+     * @return the retrieved customer
+     */
+    Customer getCustomerByNumber(String customerNumber);
 
+    /**
+     * Searches for customers by last name
+     * @param lastName the last name to search for
+     * @return a list of customers matching the last name
+     */
     List<Customer> searchCustomersByLastName(String lastName);
-
-    List<Customer> getAllCustomers();
 }
-```
-
-```java
